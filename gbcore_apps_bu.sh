6f01931c21c1c489fdcec58768b524fdfe18a203
zuls <zularbine@gmail.com>
2016-05-24 17:44:12 +0600

    Update gbcore_apps_bu.sh

--- gbcore_apps_bu.sh
+++ gbcore_apps_bu.sh
@@ -10,7 +10,7 @@ storageContainer="containerr_name"
 fecha=`date +"%d-%m-%Y"`
 
 #Backup location
-backup_location="/mnt/resource/gbcore_backup/apps"
+backup_location="/mnt/resource/apps_backup/apps"
 #Backup file name 
 backupfile="$backup_location/TEST-application-$fecha.tar.bz2"
 
@@ -20,22 +20,21 @@ echo $backupfile
 email_msg="/home/auto_backup/email_text_apps"
 
 #Server name
-server="gbuilder.com"
+server="myserver.com"
 
 #email receipients
-#EMAILS="zularbine@arditesbd.com,harri@groupbuilder.fi,marko@groupbuilder.fi"
 EMAILS="zularbine@arditesbd.com,zularbine@gmail.com"
 
 # Backup and gzip the directory
 #tar jcvf $backupfile /opt/glassfish4/glassfish/domains/domain1/applications/
-tar jcvf $backupfile /mnt/resource/gbcore_backup/mysql/
+tar jcvf $backupfile /mnt/resource/apps_backup/mysql/
 
 if [ $? -eq 0 ]; then
    echo "Application folder backup is taken at : $(date). \nBackup files are located in: $backup_location\n\n\nHave a good day!\nroot, $server" > $email_msg
-   mail -s "(gbuilder.com)- GBCore Application folder backup on $fecha is Successful! " $EMAILS < $email_msg
+   mail -s "($server)- Application folder backup on $fecha is Successful! " $EMAILS < $email_msg
 else
    echo "Application folder backup is FAILED at : $(date). \nPlease fix it. \n\n\nHave a good day!\nroot, $server" > $email_msg
-   mail -s "(gbuilder.com)- GBCore Application folder backup on $fecha is FAILED! " $EMAILS < $email_msg
+   mail -s "($server)- GBCore Application folder backup on $fecha is FAILED! " $EMAILS < $email_msg
 fi
 
 # Rotate the backups, delete older than Five days
